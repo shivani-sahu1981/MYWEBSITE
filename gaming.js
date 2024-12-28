f0224e6200b55ea060c5284e7170358cee1edc09
shivani-sahu1981 <shivanisahu1981@gmail.com>
2024-12-28 20:18:07 +0530

    box

--- gaming.js
+++ gaming.js
@@ -23,8 +23,28 @@ let faqBox = document.querySelectorAll('.faq .box-container .box');
 faqBox.forEach(box =>{
 
     box.onclick = () =>{
-        let content = box.querySelector('content');
-        let icon = box.querySelector(i);
+
+        let content = box.querySelector('.content');
+        let icon = box.querySelector('i');
+
+        if(content.classList.contains('active')){
+            content.classList.remove(active);
+        }else{
+            document.querySelectorAll('.faq .box-container .box .content').forEach(contents =>{
+                    contents.classList.remove('active');
+                });
+                content.classList.add(active);
+        }
+
+        if(icon.classList.contains('fa-minus')){
+            icon.classList.replace('fa-minus', 'fa-plus');
+        }else{
+            document.querySelectorAll('.faq .box-container .box  .i .fa-minus').forEach(icons =>{
+                    icons.classList.replace('fa-minus', 'fa-plus');
+                });
+                icon.classList.replace('fa-plus', 'fa-minus');
+        }
+
     }
     
 });
