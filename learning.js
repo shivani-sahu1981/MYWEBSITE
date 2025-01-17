9eb4d036eb3e36f9390cddf2c0cfc239a80cdff9
shivani-sahu1981 <shivanisahu1981@gmail.com>
2025-01-17 19:24:21 +0530

    g

---
+++ learning.js
@@ -0,0 +1,23 @@
+// let str1 = "shivani ";
+// let str2 = "sahu";
+
+// let res ="hello " + str1 + str2;
+// console.log(res);   
+
+// replace the char
+
+// let str = "hello"
+// console.log(str.replace("h", "y"));
+
+
+//  print the character
+
+// let str = "IloveJS";
+// console.log(str.charAt(5));
+
+// let str = "IloveJs";
+// console.log(str[0]);
+
+let str = "IloveJS";
+str = str.replace("I", "s");
+console.log(str);
