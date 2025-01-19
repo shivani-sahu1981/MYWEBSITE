8869ec831d9e4071e194395a2e647b231918ae09
shivani-sahu1981 <shivanisahu1981@gmail.com>
2025-01-19 22:48:02 +0530

    fs

--- learning.js
+++ learning.js
@@ -37,40 +37,52 @@
 // console.log(age);
 // console.log(age.length);
 
-let names = ["harry", "radhe", "ruhi", "shivi","aayu"];
+// let names = ["harry", "radhe", "ruhi", "shivi","aayu"];
 
 // for loop
 
-for(let i=0; i < names.length; i++){
-    console.log(names[i]);
-}
+// for(let i=0; i < names.length; i++){
+//     console.log(names[i]);
+// }
 
 // for of 
 
-for (let name of names){
-    console.log(name);
-}
-
-let cities = ["delhi", "pune","mumbai", "banglore","hyderabad", "noida"];
-
-for(let city of cities){
-    console.log(city.toUpperCase());
-}
-
-let marks = [85, 97,44,37,76,60];
-let sum = 0;
-for(let val of marks){
-    sum += val;
-}
-
-let avg = sum / marks.length;
-console.log(`avg marks of the class = ${avg}`);
+// for (let name of names){
+//     console.log(name);
+// }
 
 
-let prices = [250, 645, 300, 900, 50]
 
-for (let i = 0; i < prices.length; i++){
-    let offer = prices[i] / 10;
-    prices[i] -= offer;
-}
-console.log(prices);
+// let cities = ["delhi", "pune","mumbai", "banglore","hyderabad", "noida"];
+
+// for(let city of cities){
+//     console.log(city.toUpperCase());
+// }
+
+
+
+// let marks = [85, 97,44,37,76,60];
+// let sum = 0;
+// for(let val of marks){
+//     sum += val;
+// }
+
+
+
+// let avg = sum / marks.length;
+// console.log(`avg marks of the class = ${avg}`);
+
+
+// let prices = [250, 645, 300, 900, 50]
+
+// for (let i = 0; i < prices.length; i++){
+//     let offer = prices[i] / 10;
+//     prices[i] -= offer;
+// }
+// console.log(prices);
+
+// arrays methods
+let foodItems = ["potato","tomato","onion","peas"];
+foodItems.push("chips","burger","paneer");
+console.log(foodItems);
+
